e87b6200462c4c85c9b75533952ff46db5469d8e
Centauri992 <83069853+Centauri992@users.noreply.github.com>
2025-07-14 22:58:07 +0200

    Add files via upload

--- startup-script.sh
+++ startup-script.sh
@@ -1,38 +1,36 @@
 #!/bin/bash
 set -e
 
-# Update and install dependencies
+# 1. Update and install dependencies
 apt-get update
 apt-get install -y python3-pip git python3-venv
 
-cd /root/
-
-# Clone your repo
+# 2. Clone your repo
 REPO="smpc"
 git clone https://github.com/Centauri992/$REPO.git
-cd /root/$REPO
+cd $REPO
 
-# Set up virtualenv
+# 3. Set up virtualenv
 python3 -m venv smpc-venv
 source smpc-venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# Fetch instance attributes
+# 4. Fetch instance attributes
 PARTY_ID="$(curl -s http://metadata.google.internal/computeMetadata/v1/instance/attributes/party_id -H 'Metadata-Flavor: Google')"
 BUCKET="$(curl -s http://metadata.google.internal/computeMetadata/v1/instance/attributes/bucket -H 'Metadata-Flavor: Google')"
 TOTAL="$(curl -s http://metadata.google.internal/computeMetadata/v1/instance/attributes/total -H 'Metadata-Flavor: Google')"
 LOCAL_IP="$(hostname -I | awk '{print $1}')"
 
-# Register IP for coordination
+# 5. Register IP for coordination (one file per party)
 echo "$LOCAL_IP" | gsutil cp - gs://$BUCKET/party-$PARTY_ID
 
-# Wait for all 3 IPs
+# 6. Wait for all 3 IPs to appear
 while [ $(gsutil ls gs://$BUCKET/party-* | wc -l) -lt $TOTAL ]; do
   sleep 2
 done
 
-# Read all party IPs
+# 7. Read all party IPs, build MPyC -P arguments
 HOSTS=""
 for i in $(seq 0 $(($TOTAL - 1))); do
   IP=$(gsutil cat gs://$BUCKET/party-$i)
@@ -40,35 +38,42 @@ for i in $(seq 0 $(($TOTAL - 1))); do
 done
 HOSTS=${HOSTS::-1}
 
-echo "PARTY $PARTY_ID running on $LOCAL_IP with hosts: $HOSTS"
+PORT=12345  # Use any available port; must be same for all
+PARTY_ADDRS=""
+for ip in $(echo $HOSTS | tr ',' ' '); do
+  PARTY_ADDRS="$PARTY_ADDRS -P $ip:$PORT"
+done
+
+echo "PARTY $PARTY_ID running on $LOCAL_IP with party addresses: $PARTY_ADDRS"
 
 BATCH=10
 TOTAL_IMAGES=10000
-cd /root/$REPO
+cd ~/smpc  # If in root, use /root/smpc; otherwise, use $HOME/smpc
+
 source smpc-venv/bin/activate
 
-# Loop over all protocols
+# 8. Loop over all protocols
 for PROTO in 0 1 2; do
   OUT="results_party${PARTY_ID}_k${PROTO}.txt"
   echo "Protocol d_k_star = $PROTO" > $OUT
   for OFFSET in $(seq 0 $BATCH $((TOTAL_IMAGES-1))); do
-    python3 smpc.py -M $TOTAL -P $PARTY_ID -H $HOSTS -b $BATCH -o $OFFSET -d $PROTO >> $OUT
+    python3 smpc.py -M $TOTAL -I $PARTY_ID $PARTY_ADDRS -b $BATCH -o $OFFSET -d $PROTO >> $OUT
   done
   gsutil cp $OUT gs://$BUCKET/
 done
 
-# Toft's protocol
+# 9. Toft's protocol
 OUT="results_party${PARTY_ID}_toft.txt"
 echo "Protocol Toft (built-in MPyC)" > $OUT
 for OFFSET in $(seq 0 $BATCH $((TOTAL_IMAGES-1))); do
-  python3 smpc.py -M $TOTAL -P $PARTY_ID -H $HOSTS -b $BATCH -o $OFFSET --no-legendre >> $OUT
+  python3 smpc.py -M $TOTAL -I $PARTY_ID $PARTY_ADDRS -b $BATCH -o $OFFSET --no-legendre >> $OUT
 done
 gsutil cp $OUT gs://$BUCKET/
 
-# Write done marker
+# 10. Write done marker
 gsutil cp <(echo "done") gs://$BUCKET/party-${PARTY_ID}-done
 
-# Wait for all to finish, then shutdown
+# 11. Wait for all to finish, then shutdown
 while [ $(gsutil ls gs://$BUCKET/party-*-done | wc -l) -lt $TOTAL ]; do
   sleep 5
 done
